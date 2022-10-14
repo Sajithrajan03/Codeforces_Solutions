b9028f0d8106278b227b810cf79f55c928039b33
Sajith Rajan P <93327106+Sajithrajan03@users.noreply.github.com>
2022-10-14 22:22:26 +0530

    Update and rename sum.cpp to A_Sum.cpp

--- Codeforces Round #837(Div. 4)/A_Sum.cpp
+++ Codeforces Round #837(Div. 4)/A_Sum.cpp
@@ -12,9 +12,7 @@ int main(){
     {   
         int a,b,c;
         cin>>a>>b>>c;
-        if (a==0 ||b==0||c==0) 
-        {cout<<"YES"<<endl;
-        continue;}
+        
         if (a+b==c || b+c==a|| c+a==b) cout<<"YES"<<endl;
         else cout<<"NO"<<endl;
     }
